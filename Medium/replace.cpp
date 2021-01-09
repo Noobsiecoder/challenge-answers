d3e35505d646144112f216e77dee30b0d1da7ea7
Rubakpreyan <rubakkeerthi123@gmail.com>
2021-01-09 21:48:32 +0530

    completed medium level(updated)

--- Medium/replace.cpp
+++ Medium/replace.cpp
@@ -1,3 +1,4 @@
+
 #include <bits/stdc++.h>
 #include<iostream>
 
@@ -5,7 +6,7 @@ using namespace std;
 int main() 
 {   
     int a[127]={0}; // initialized as 0 inorder to find the count of each letter
-	string s="Mississippi";
+	string s="Dad";
 	
 	// Finding the lenght of the string 
 	int x = s.length();
@@ -15,7 +16,7 @@ int main()
        
     for(int i=0;i<x;i++)
         
-        if(a[(char)tolower(s[i] - 'a')] > 1) //checking whether the letter is repeated more than once
+        if(a[(char)tolower(s[i])-'a'] > 1) //checking whether the letter is repeated more than once
             
             s[i] = '*';  // converting that letter into a '*'
 	
