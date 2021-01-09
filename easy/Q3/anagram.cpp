a242ba93f6c08167dc960e826cd5c5097c9bbfb0
Abhishek Sriram <68271853+Noobsiecoder@users.noreply.github.com>
2021-01-09 12:41:12 +0530

    Update anagram.cpp

--- easy/Q3/anagram.cpp
+++ easy/Q3/anagram.cpp
@@ -1,8 +1,14 @@
 #include <bits/stdc++.h>
 
 // String function to sort a given list
-std::string sorted_string(std::string sort_str)
-{
+std::string sorted_string(std::string str)
+{   
+    // New string to store the variable {str} in lowercase
+    std::string sort_str = "";
+    // Loop to convert str to lower-case and store them in a new variable {lowercase_string}
+    for (int i = 0; i <= str.length() - 1; i++)
+        sort_str += tolower(str[i]);
+    // Sorting {sort_str}
     std::sort(sort_str.begin(), sort_str.end());
     return sort_str;
 }
