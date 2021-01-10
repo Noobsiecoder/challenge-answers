2d9c39fefd3603933f4e1fa6c092dd440899e9e1
Abhishek Sriram <68271853+Noobsiecoder@users.noreply.github.com>
2021-01-10 22:58:29 +0530

    completed half of difficult-challenges

--- difficult/Q4/arraydifference.py
+++ difficult/Q4/arraydifference.py
@@ -6,4 +6,3 @@ def array_difference(list1, list2):
 print(array_difference([1, 2], [2]) == [1]) # True
 print(array_difference([1, 2, 3], [1, 8, 3]) == [2]) # True
 print(array_difference([], [3, 4]) == []) # True
-#
