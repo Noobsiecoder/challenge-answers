3d7ec80083eef03e4356407279a416109a5c6d67
Abhishek Sriram <68271853+Noobsiecoder@users.noreply.github.com>
2021-01-13 12:05:44 +0530

    completed tough-challenges

--- tough/Q2/dictdecode.js
+++ tough/Q2/dictdecode.js
@@ -1,6 +1,12 @@
-const decrypt = (string, shift) => {};
+// Function to decrypt the encrypted data
+const decrypt = (string, shift) =>
+  string
+    .split("")
+    .map((char) =>
+      String.fromCharCode(char.charCodeAt(0) ^ (shift + (26 % 96)))
+    )
+    .join("");
 
-// Don't edit the below lines!
 let person_info = {
   '*%>?8"-!)': '#$"',
   ' -?8"-!)': "#)",
@@ -9,26 +15,18 @@ let person_info = {
 };
 
 let cracked_data = {};
-// Don't edit the above lines!
+// To store the decrypted data in {cracked_data} 
+Object.keys(person_info).forEach(
+  (key) => (cracked_data[decrypt(key, 50)] = decrypt(person_info[key], 50))
+);
+// To print key and its correponding values in {cracked_data} 
+Object.keys(cracked_data).forEach((key) =>
+  console.log(`${key}: ${cracked_data[key]}`)
+);
 
 /*
- * In the given object, an unknown person's credentials are given. The problem is, it is encrypted. Decrypt it by reverse engineering the encryption algorithm used. 
- 
- * After decrypting store it in another object named {cracked_data}
- */
-
-/*
-  * Encryption Algorithm :-
-        * From a string, a character's ASCII value is taken bitwise XOR with given shift key where, shift key is first added with the total numbers of alphabets in english language and also taken modulus with the ASCII value of the character from 5 places after. 
-
-  * The {shift} value is a number which lies between :
-        * 1 to 100
-        * {shift} % {secret_num} = 16 and {shift} % {(secret_num * 2)} = 16 
-        * The {secret_value} value whose binary signed 2's complement is {0000000000010001}
-
-  * Psuedo-code for Encryption Algorithm :
-        * for {character} inside {string}
-            * ASCII_value of {character} bitwise XOR with ({shift} add {total_alphabets_in_english} modulo with {ASCII_value of {character after 5places})
-            * Join with {Encrypted_string}
-        * return {Encrypted_string}
+ * first_name: John
+ * last_name: Doe
+ * email: slayjohndoeplayer186@gmail.com
+ * password: howdidyoufindit?
  */
--- tough/Q2/dictdecode.py
+++ tough/Q2/dictdecode.py
@@ -1,36 +1,20 @@
 def decrypt(string, shift):
-    pass
+    """ Function to decrypt the encrypted data """
+    return "".join([(chr(ord(char) ^ (shift + 26 % 97))) for char in string])
 
 
 if __name__ == "__main__":
-    ''' Don't edit the below lines! '''
     person_info = {
         '*%>?8"-!)': '#$"',
         ' -?8"-!)': "#)",
         ")!-% ": "? -5&#$\"(#)< -5)>}tz+!-% b/#!", # If this line is highlighted in red color, ignore it (No issues)
         "<-??;#>(": "$#;(%(5#9*%\"(%8s",
     }
+
     cracked_data = {}
-    ''' Don't edit the above lines! '''
+    for key, value in person_info.items():
+        # To store the decrypted data in {cracked_data} 
+        cracked_data[decrypt(key, 50)] = decrypt(value, 50)
 
-    '''
-    - In the given dictionary, an unknown person's credentials are given. The problem is, it is encrypted. Decrypt it by reverse engineering the encryption algorithm used.
-    
-    - After decrypting store it in another dictionary named {cracked_data}
-    '''
+    print(cracked_data == {'first_name': 'John', 'last_name': 'Doe', 'email': 'slayjohndoeplayer186@gmail.com', 'password': 'howdidyoufindit?'}) # True
 
-    '''
-    Encryption Algorithm :-
-        - From a string, a character's ASCII value is taken bitwise XOR with given shift key where, shift key is first added with the total numbers of alphabets in english language and also taken modulus with the ASCII value of the character from 5 places after. 
-
-        - The {shift} value is a number which lies between :
-            - 1 to 100
-            - {shift} % {secret_num} = 16 and {shift} % {(secret_num * 2)} = 16 
-            - The {secret_value} value whose binary signed 2's complement is {0000000000010001}
-
-        - Psuedo-code for Encryption Algorithm :
-            - for {character} inside {string}
-                - ASCII_value of {character} bitwise XOR with ({shift} add {total_alphabets_in_english} modulo with {ASCII_value of {character} add {5})
-                - Join with {Encrypted_string}
-            - return {Encrypted_string}
-    ''' 
